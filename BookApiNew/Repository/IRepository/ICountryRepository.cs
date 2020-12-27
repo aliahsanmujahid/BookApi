6a9cca70d606508d99c85e07bf856c562a27cf75
aliahsanmujahid <ahsanalimujahid@gmail.com>
2020-12-27 23:48:57 +0600

    first commit

---
+++ BookApiNew/Repository/IRepository/ICountryRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookApiNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiNew.Repository.IRepository
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int countryId);
+        Country GetCountryOfAnAuthor(int authorId);
+        ICollection<Author> GetAuthorsFromACountry(int countryId);
+        bool CountryExists(int countryId);
+        bool IsDuplicateCountryName(int countryId, string countryName);
+
+        bool CreateCountry(Country country);
+        bool UpdateCountry(Country country);
+        bool DeleteCountry(Country country);
+        bool Save();
+
+
+    }
+}
---
+++ BookApiNew/Repository/IRepository/IReviewRepository.cs
@@ -0,0 +1,24 @@
+﻿
+using BookApiNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiNew.Repository.IRepository
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfABook(int bookId);
+        Book GetBookOfAReview(int reviewId);
+        bool ReviewExists(int reviewId);
+
+        bool CreateReview(Review review);
+        bool UpdateReview(Review review);
+        bool DeleteReview(Review review);
+        bool DeleteReviews(List<Review> reviews);
+        bool Save();
+    }
+}
